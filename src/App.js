358c358921fdace1c83061b70d55ce52fd3da78d
Simon Anguish <simon.anguish@gmail.com>
2020-03-17 18:48:15 -0400

    Tutorial Code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import './App.scss';
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        characters: [],
+    }
+
+    removeCharacter = index => {
+        const { characters } = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+
+    handleSubmit = character => {
+        this.setState({
+            characters: [...this.state.characters, character]
+        })
+    }
+
+    render() {
+        const { characters } = this.state
+
+        return (
+            <div className="App">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
