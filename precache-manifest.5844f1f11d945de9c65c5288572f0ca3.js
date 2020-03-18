e9bd83eff4053beac84e489bb7684dc5de494d10
Simon Anguish <simon.anguish@gmail.com>
2020-03-18 16:53:57 -0400

    Updates

---
+++ precache-manifest.5844f1f11d945de9c65c5288572f0ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632c961cab8189e62fc64830ad857bb9",
+    "url": "/raincloud/index.html"
+  },
+  {
+    "revision": "dbedf560b5e87d21ec15",
+    "url": "/raincloud/static/css/main.fa92c0b1.chunk.css"
+  },
+  {
+    "revision": "150a39fbd838eac7ab9e",
+    "url": "/raincloud/static/js/2.b5a9818f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/raincloud/static/js/2.b5a9818f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbedf560b5e87d21ec15",
+    "url": "/raincloud/static/js/main.b95886fa.chunk.js"
+  },
+  {
+    "revision": "0c9a2f2fc4872bd989f8",
+    "url": "/raincloud/static/js/runtime-main.0678d56d.js"
+  }
+]);
